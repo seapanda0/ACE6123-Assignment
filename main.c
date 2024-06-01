95f017336510b8a941e179b16a32eaa6d06dbe33
seapanda0 <1211112330@student.mmu.edu.my>
2024-06-01 18:37:13 +0800

    Load file into linked list

--- main.c
+++ main.c
@@ -5,7 +5,20 @@
 #include <regex.h>
 
 #define FILENAME "dataset.txt"
-#define REGEX_EXPRESSION "^.{2,3}\\s[0-9]*,[A-Z]+,[A-Z]+,[0-9]+,[0-9]{4},[0-9]+,[0-9]{1},\n*.*$"
+#define REGEX_EXPRESSION "^.{2,3}\\s[0-9]*,[A-Z]+,[A-Z]+,[0-9]+,[0-9]{4},[0-9]+,[0-9]{1},\n*.*$" // Narrow down acceptable char lengths to avoid overflow
+
+typedef struct dataSet
+{
+    char flightNumber[20];
+    char origin[5];
+    char destination[5];
+    short capacity;
+    short departureHour;
+    short departureMinutes;
+    int price;
+    short stops;
+    struct dataSet *nextNode;
+}dataSet;
 
 // Return true lines in the files and return -1 if a empty line is found 
 int trueLinecount(FILE *fp){
@@ -30,6 +43,7 @@ int trueLinecount(FILE *fp){
             // printf("%s", line);
         }
     }
+    printf("File Structure is Correct! File has %d lines\n", trueLine);
     rewind(fp);
     return trueLine;
 }
@@ -48,14 +62,86 @@ int validateFile(FILE  *fp){
             regfree(&regex);
             fclose(fp);
             fprintf(stderr, "Error: format error at line %d of %s\n", i, FILENAME);
-            exit(EXIT_FAILURE);        }
+            exit(EXIT_FAILURE);        
+        }
         i++;
     }
+    printf("Content Validation Successful!\n");
     rewind(fp);
     regfree(&regex);
 }
 
-int main (void){
+// Return 0 if time is correct, split time in String to two hour and minutes in short
+int validateTime(char time[], short *hour, short *minutes){
+    char temp[3];
+    strncpy(temp, time, 2), temp[2]='\0';
+    *hour = atoi(temp);
+    strncpy(temp, time+2, 2), temp[2]='\0';
+    *minutes = atoi(temp);
+
+    if (*minutes>= 0 && *minutes <= 59 && *hour>=0 && *hour<=23){
+        return 0;
+    }else{
+        return 1;
+    }
+}
+
+// Load file into a linked list memory
+dataSet *loadFile(FILE *fp, int lineCount){
+    char line[256], time[5];
+    dataSet temp;
+    int err, i = 0;
+
+    dataSet *curr = (dataSet *)calloc(1, sizeof(dataSet));
+    dataSet *head = curr, *tail;
+    // printf("%p\n", head);
+    //jump to first newline
+    while( fgetc(fp) != '\n' ){}
+
+    for (i = 1; i <= lineCount-1; i++){
+
+        fgets(line, sizeof(line), fp);
+        sscanf(line, "%19[^,],%5[^,],%5[^,],%hd,%4[^,],%d,%hd", curr->flightNumber, curr->origin, curr->destination,
+               &(curr->capacity), time, &(curr->price), &(curr->stops));
+
+        err = validateTime(time, &(curr->departureHour), &(curr->departureMinutes));
+
+        if (err){
+            fclose(fp);
+            fprintf(stderr, "Error: Date Format Error at line %d of %s\n",i+1, FILENAME);
+            exit(EXIT_FAILURE);
+        }
+        if (i != lineCount -1){
+            curr->nextNode = (dataSet *)calloc(1, sizeof(dataSet));
+            curr = curr->nextNode;
+        }
+        // printf("%d\n", i);
+    }
+    // curr = head;
+    // printf("1 %s %p\n", curr->flightNumber, curr->nextNode);
+    // curr = curr->nextNode;
+    // printf("2 %s %p\n", curr->flightNumber, curr->nextNode);
+    // curr = curr->nextNode;    
+    // printf("3 %s %p\n", curr->flightNumber, curr->nextNode);
+    // curr = curr->nextNode;
+    // printf("4 %s %p\n", curr->flightNumber, curr->nextNode);
+    // curr = curr->nextNode;
+    // printf("5 %s %p\n", curr->flightNumber, curr->nextNode);
+    // curr = curr->nextNode;    
+    // printf("6 %s %p\n", curr->flightNumber, curr->nextNode);
+    // curr = curr->nextNode;
+    // printf("7 %s %p\n", curr->flightNumber, curr->nextNode);
+    // curr = curr->nextNode;
+    // printf("8 %s %p\n", curr->flightNumber, curr->nextNode);
+    // curr = curr->nextNode;    
+    // printf("9 %s %p\n", curr->flightNumber, curr->nextNode);
+    // curr = curr->nextNode;
+    // printf("10 %s %p\n", curr->flightNumber, curr->nextNode);
+
+    return head;
+}
+
+int main (){
 
     FILE *fp = fopen(FILENAME, "r+");
     if (!fp){
@@ -64,6 +150,6 @@ int main (void){
     int lineCount = trueLinecount(fp);
     validateFile(fp);
 
+    dataSet *db = loadFile(fp, lineCount);
     fclose(fp);
-
 }
